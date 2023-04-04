bb4e5b72deee0d6f7bc0e44451d446f56768a06f
whitneyschwitter <whitneyschwitter@gmail.com>
2023-04-04 19:29:25 -0400

    Edits

---
+++ app.js
@@ -0,0 +1,10 @@
+//get user selected text
+const getUserSelectedText = () => {
+    return window.getSelection().toString();
+}
+
+//detect if user is in dark mode
+const isDarkMode = window.matchMedia('(preferes-color-scheme: dark').matches; 
+
+//take user back to the top
+const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'})
