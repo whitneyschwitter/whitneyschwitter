ba2482eacfe8daeecb096145e00e25a2735ba6b6
whitneyschwitter <whitneyschwitter@gmail.com>
2023-06-19 16:54:20 -0400

    sticky

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ const isDarkMode = window.matchMedia('(preferes-color-scheme: dark').matches;
 
 //take user back to the top
 const toTop = () => window.scrollTo({top: 0, behavior: 'smooth'})
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
